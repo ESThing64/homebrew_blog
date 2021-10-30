dd606fd3ef2282b58c61ebdf1e00b794d340f93e
ESThing64 <89550738+ESThing64@users.noreply.github.com>
2021-10-29 22:37:16 -0500

    added the login page

--- controllers/api/homeRoutes.js
+++ controllers/api/homeRoutes.js
@@ -11,4 +11,15 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/login', async (req, res) => {
+  try {
+   
+    
+    res.render('login');
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
 module.exports = router;
